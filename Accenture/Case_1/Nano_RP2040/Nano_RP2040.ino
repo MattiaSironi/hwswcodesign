9d258ccfdbf9988224acc8e416319a9a0ca98527
MattiaSironi <mattia1.sironi@mail.polimi.it>
2023-10-14 15:23:06 +0200

    refactored some variables to defines; added states

--- Accenture/Case_1/Nano_RP2040/Nano_RP2040.ino
+++ Accenture/Case_1/Nano_RP2040/Nano_RP2040.ino
@@ -1,6 +1,10 @@
 #include <Arduino_LSM6DSOX.h>
-unsigned long delay_ms, delay_sum_ms, ta_ms;
+unsigned long delay_ms, delay_sum_ms;
 int max_t, min_t, sum, n_temperature;
+enum state_enum {NORMAL, ALERT, FAULT};
+state_enum state; 
+#define MAX_TMP 30
+#define TA_MS 60000
 void setup() {
   // put your setup code here, to run once:
 if (!IMU.begin()) {
@@ -9,11 +13,19 @@ if (!IMU.begin()) {
   
   }
   delay_ms = 10000;
-  ta_ms = 60000;
-  reset_temperature(); 
+  reset_temperature();
+  state = NORMAL;
 }
 
 void loop() {
+  switch (state) {
+    case NORMAL: normal(); break;
+    case ALERT: alert(); break;
+    default: fault(); break;
+  }
+}
+
+void normal() {
   // put your main code here, to run repeatedly:
   delay(delay_ms);
   if (IMU.temperatureAvailable())
@@ -24,12 +36,13 @@ void loop() {
     Serial.print("LSM6DSOX Temperature = ");
     Serial.print(temperature_deg);
     Serial.println(" °C");
+    if (temperature_deg <= MAX_TMP) {
     n_temperature++;
     sum+=temperature_deg;
     if (temperature_deg > max_t) max_t = temperature_deg;
     if (temperature_deg < min_t) min_t = temperature_deg;
     delay_sum_ms+=delay_ms;
-    if (  delay_sum_ms >= ta_ms) {
+    if (  delay_sum_ms >= TA_MS) {
       float avg = (float)sum / (float)n_temperature;
       Serial.print("Recap last minute: MAX = ");
       Serial.print(max_t);
@@ -43,6 +56,10 @@ void loop() {
       reset_temperature();
     }
   }
+  else {
+    state = ALERT;
+  }
+  }
 
 }
 
@@ -52,4 +69,14 @@ void reset_temperature(){
   sum = 0;
   n_temperature= 0;
   delay_sum_ms = 0;
+}
+
+void alert() {
+  Serial.println("ALERT");
+  delay(500);
+}
+
+void fault() {
+  Serial.println("FAULT");
+  delay(500);
 }
