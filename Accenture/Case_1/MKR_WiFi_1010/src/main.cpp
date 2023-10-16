2d7b3cdf150ff3844a67d55c565929e4d50d7df8
tomaz-suller <tomaz.suller@usp.br>
2023-10-16 20:14:13 +0200

    Format source files

--- Accenture/Case_1/MKR_WiFi_1010/src/main.cpp
+++ Accenture/Case_1/MKR_WiFi_1010/src/main.cpp
@@ -21,12 +21,14 @@ unsigned long lastMillis = 0;
 
 void messageCallback(String &topic, String &payload);
 
-void set_up_network() {
+void set_up_network()
+{
   SerialUSB.println("Setting up network connection");
 
   WiFi.begin(ssid, wifi_password);
   SerialUSB.println("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     SerialUSB.print(".");
     delay(1000);
   }
@@ -35,35 +37,41 @@ void set_up_network() {
   mqtt_client.begin(host, port, wifi_client);
   mqtt_client.onMessage(messageCallback);
   SerialUSB.println("Connecting to MQTT broker");
-  while (!mqtt_client.connect("mkr1000", username, mqtt_password)) {
+  while (!mqtt_client.connect("mkr1000", username, mqtt_password))
+  {
     SerialUSB.print(".");
     delay(500);
   }
   SerialUSB.println("\nMQTT connection successful!");
 }
 
-void messageCallback(String &topic, String &payload) {
-  SerialUSB.println("Received message '"+payload+"' from topic "+topic);
+void messageCallback(String &topic, String &payload)
+{
+  SerialUSB.println("Received message '" + payload + "' from topic " + topic);
 }
 
-void setup() {
+void setup()
+{
   SerialUSB.begin(9600);
-  while (!SerialUSB.available());
+  while (!SerialUSB.available())
+    ;
   Serial1.begin(9600);
-  while (!Serial1.available());
   set_up_network();
   mqtt_client.subscribe("teamE/test/receive");
 }
 
-void loop() {
+void loop()
+{
   mqtt_client.loop();
-  if (!mqtt_client.connected()) set_up_network();
-  if (millis() - lastMillis > 1000) {
+  if (!mqtt_client.connected())
+    set_up_network();
+  if (millis() - lastMillis > 1000)
+  {
     lastMillis = millis();
     mqtt_client.publish("teamE/test/mkr1000-heartbeat", "");
   }
 
   Serial1.readBytes(received_message_buffer, sizeof(message_t));
   received_message = (message_t *)received_message_buffer;
-  SerialUSB.println("Received MAX temp is "+received_message->max_temp);
+  SerialUSB.println("Received MAX temp is " + received_message->max_temp);
 }
--- Accenture/Case_1/Nano_RP2040/src/main.cpp
+++ Accenture/Case_1/Nano_RP2040/src/main.cpp
@@ -12,36 +12,56 @@ void debug();
 unsigned long delay_ms, delay_sum_ms;
 int max_t, min_t, sum, n_temperature;
 
-enum state_enum {NORMAL, ALERT, FAULT, DEBUG};
-state_enum state; 
+enum state_enum
+{
+  NORMAL,
+  ALERT,
+  FAULT,
+  DEBUG
+};
+state_enum state;
 
 #define MAX_TMP 30
 #define TA_MS 60000
 
-void setup() {
+void setup()
+{
   Serial1.begin(9600);
   // put your setup code here, to run once:
-if (!IMU.begin()) {
+  if (!IMU.begin())
+  {
     Serial.println("Failed to initialize IMU!");
-    while (1);
-  
+    while (1)
+      ;
   }
   delay_ms = 10000;
   reset_temperature();
   state = NORMAL;
-  if (state == DEBUG) pinMode(LED_BUILTIN, OUTPUT);
+  if (state == DEBUG)
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  switch (state) {
-    case DEBUG : debug(); break;
-    case NORMAL: normal(); break;
-    case ALERT: alert(); break;
-    default: fault(); break;
+void loop()
+{
+  switch (state)
+  {
+  case DEBUG:
+    debug();
+    break;
+  case NORMAL:
+    normal();
+    break;
+  case ALERT:
+    alert();
+    break;
+  default:
+    fault();
+    break;
   }
 }
 
-void normal() {
+void normal()
+{
   // put your main code here, to run repeatedly:
   delay(delay_ms);
   if (IMU.temperatureAvailable())
@@ -52,66 +72,74 @@ void normal() {
     Serial.print("LSM6DSOX Temperature = ");
     Serial.print(temperature_deg);
     Serial.println(" °C");
-    if (temperature_deg <= MAX_TMP) {
-    n_temperature++;
-    sum+=temperature_deg;
-    if (temperature_deg > max_t) max_t = temperature_deg;
-    if (temperature_deg < min_t) min_t = temperature_deg;
-    delay_sum_ms+=delay_ms;
-    if (  delay_sum_ms >= TA_MS) {
-      float avg = (float)sum / (float)n_temperature;
+    if (temperature_deg <= MAX_TMP)
+    {
+      n_temperature++;
+      sum += temperature_deg;
+      if (temperature_deg > max_t)
+        max_t = temperature_deg;
+      if (temperature_deg < min_t)
+        min_t = temperature_deg;
+      delay_sum_ms += delay_ms;
+      if (delay_sum_ms >= TA_MS)
+      {
+        float avg = (float)sum / (float)n_temperature;
 
-      message_to_send = {
-        1,
-        (uint8_t)NORMAL,
-        true, 
-        max_t,
-        min_t,
-        avg,
-      };
+        message_to_send = {
+            1,
+            (uint8_t)NORMAL,
+            true,
+            max_t,
+            min_t,
+            avg,
+        };
 
-      Serial.print("Recap last minute: MAX = ");
-      Serial.print(message_to_send.max_temp);
-      Serial.print(" °C");
-      Serial.print(", MIN = ");
-      Serial.print(message_to_send.min_temp);
-      Serial.print(" °C");
-      Serial.print(", AVG = ");
-      Serial.print(message_to_send.avg_temp);
-      Serial.println(" °C");
+        Serial.print("Recap last minute: MAX = ");
+        Serial.print(message_to_send.max_temp);
+        Serial.print(" °C");
+        Serial.print(", MIN = ");
+        Serial.print(message_to_send.min_temp);
+        Serial.print(" °C");
+        Serial.print(", AVG = ");
+        Serial.print(message_to_send.avg_temp);
+        Serial.println(" °C");
 
-      Serial.write((uint8_t *)&message_to_send, sizeof(message_t));
+        Serial.write((uint8_t *)&message_to_send, sizeof(message_t));
 
-      reset_temperature();
+        reset_temperature();
+      }
+    }
+    else
+    {
+      state = ALERT;
     }
   }
-  else {
-    state = ALERT;
-  }
-  }
-
 }
 
-void reset_temperature(){
+void reset_temperature()
+{
   max_t = 0;
   min_t = 200;
   sum = 0;
-  n_temperature= 0;
+  n_temperature = 0;
   delay_sum_ms = 0;
 }
 
-void alert() {
+void alert()
+{
   Serial.println("ALERT");
   delay(500);
 }
 
-void fault() {
+void fault()
+{
   Serial.println("FAULT");
   delay(500);
 }
-void debug() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+void debug()
+{
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(1000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+  delay(1000);                     // wait for a second
 }
