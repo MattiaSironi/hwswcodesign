36535bb6294c1f40be07b17f17813371ee2d516d
MattiaSironi <mattia1.sironi@mail.polimi.it>
2023-10-18 01:41:22 +0200

    minor : small modifications and code cleaning for more readability and more modularity. TODO : state switching logic

--- Accenture/Case_1/Nano_RP2040/src/main.cpp
+++ Accenture/Case_1/Nano_RP2040/src/main.cpp
@@ -14,15 +14,16 @@ int max_t, min_t, sum, n_temperature;
 
 enum state_enum {NORMAL, ALERT, FAULT, DEBUG};
 state_enum state; 
-
+#define LINE_ID 0
 #define MAX_TMP 30
 #define TA_MS 60000
-
+#define DEBUG(x) Serial.print(x)
+#define DEBUGln(x) Serial.println(x)
 void setup() {
   Serial1.begin(9600);
   // put your setup code here, to run once:
 if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
+    DEBUGln("Failed to initialize IMU!");
     while (1);
   
   }
@@ -43,50 +44,14 @@ void loop() {
 
 void normal() {
   // put your main code here, to run repeatedly:
+  delay_ms = 10000;
   delay(delay_ms);
-  if (IMU.temperatureAvailable())
-  {
-    int temperature_deg = 0;
-    IMU.readTemperature(temperature_deg);
-
-    Serial.print("LSM6DSOX Temperature = ");
-    Serial.print(temperature_deg);
-    Serial.println(" °C");
-    if (temperature_deg <= MAX_TMP) {
-    n_temperature++;
-    sum+=temperature_deg;
-    if (temperature_deg > max_t) max_t = temperature_deg;
-    if (temperature_deg < min_t) min_t = temperature_deg;
-    delay_sum_ms+=delay_ms;
-    if (  delay_sum_ms >= TA_MS) {
-      float avg_t = (float)sum / (float)n_temperature;
-
-      message_to_send = {
-        1,
-        (uint8_t)NORMAL,
-        true, 
-        max_t,
-        min_t,
-        avg_t,
-      };
-
-      Serial.print("Recap last minute: MAX = ");
-      Serial.print(message_to_send.max_temp);
-      Serial.print(" °C");
-      Serial.print(", MIN = ");
-      Serial.print(message_to_send.min_temp);
-      Serial.print(" °C");
-      Serial.print(", AVG = ");
-      Serial.print(message_to_send.avg_temp);
-      Serial.println(" °C");
-// "id,status,max,min,avg" id = index 0, status index 2, max index 4-5, min index 7-8, avg index 10-11-12-13-14;
-      char buffer[15];
-      sprintf(buffer, "%d,%d,%d,%d,%.2f", 0, state, max_t, min_t, avg_t);
-      Serial1.write(buffer);
-
-      //Serial.write((uint8_t *)&message_to_send, sizeof(message_t));
-
-      reset_temperature();
+  retrieve_temperature();
+  // "id,status,max,min,avg" id = index 0, status index 2, max index 4-5, min index 7-8, avg index 10-11-12-13-14;
+  char buffer[15];
+  sprintf(buffer, "%d,%d,%d,%d,%.2f", LINE_ID, state, max_t, min_t, avg_t);
+  Serial1.write(buffer);
+  reset_temperature();
     }
   }
   else {
@@ -96,7 +61,37 @@ void normal() {
 
 }
 
-void reset_temperature(){
+void retrieve_temperature() {
+  if (IMU.temperatureAvailable())
+  {
+    int temperature_deg = 0;
+    IMU.readTemperature(temperature_deg);
+
+    DEBUG("LSM6DSOX Temperature = ");
+    DEBUG(temperature_deg);
+    DEBUGln(" °C");
+    if (temperature_deg > MAX_TMP) state = ALERT;
+    n_temperature++;
+    sum+=temperature_deg;
+    if (temperature_deg > max_t) max_t = temperature_deg;
+    if (temperature_deg < min_t) min_t = temperature_deg;
+    delay_sum_ms+=delay_ms;
+    if ( delay_sum_ms >= TA_MS) {
+      int avg_t = sum / n_temperature;
+
+      DEBUG("Recap last minute: MAX = ");
+      DEBUG(max_t);
+      DEBUG(" °C");
+      DEBUG(", MIN = ");
+      DEBUG(min_t);
+      DEBUG(" °C");
+      DEBUG(", AVG = ");
+      DEBUG(avg_t);
+      DEBUGln(" °C");
+
+} 
+
+void reset_temperature() {
   max_t = 0;
   min_t = 200;
   sum = 0;
@@ -105,12 +100,13 @@ void reset_temperature(){
 }
 
 void alert() {
-  Serial.println("ALERT");
+  delay_ms(1000);
+  DEBUGln("ALERT");
   delay(500);
 }
 
 void fault() {
-  Serial.println("FAULT");
+  DEBUGln("FAULT");
   delay(500);
 }
 void debug() {
