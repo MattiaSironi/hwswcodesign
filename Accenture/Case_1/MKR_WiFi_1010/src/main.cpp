b7576413beca057a1a762e22417a8350436832a4
tomaz-suller <tomaz.suller@usp.br>
2023-10-18 13:44:00 +0200

    Fix bugs and refactor

--- Accenture/Case_1/MKR_WiFi_1010/src/main.cpp
+++ Accenture/Case_1/MKR_WiFi_1010/src/main.cpp
@@ -2,28 +2,39 @@
 #include <WiFi.h>
 #include <MQTT.h>
 
-#define SERVER_ON 0
+#define SERVER_ON false
 
-enum state_enum {NORMAL, ALERT, FAULT, DEBUG};
-state_enum state[1]; 
+enum state_enum
+{
+  NORMAL,
+  ALERT,
+  FAULT,
+  DEBUG
+};
+
+state_enum state[1];
 WiFiClient wifi_client;
 MQTTClient mqtt_client;
+
 static const char *ssid = WIFI_SSID;
 static const char *wifi_password = WIFI_PASSWORD;
 static const char *host = MQTT_HOST;
 static const int port = MQTT_PORT;
 static const char *username = MQTT_USERNAME;
 static const char *mqtt_password = MQTT_PASSWORD;
+
 unsigned long lastMillis = 0;
 
 void messageCallback(String &topic, String &payload);
 
-void set_up_network() {
+void set_up_network()
+{
   SerialUSB.println("Setting up network connection");
 
   WiFi.begin(ssid, wifi_password);
   SerialUSB.println("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     SerialUSB.print(".");
     delay(1000);
   }
@@ -32,41 +43,65 @@ void set_up_network() {
   mqtt_client.begin(host, port, wifi_client);
   mqtt_client.onMessage(messageCallback);
   SerialUSB.println("Connecting to MQTT broker");
-  while (!mqtt_client.connect("mkr1000", username, mqtt_password)) {
+  while (!mqtt_client.connect("mkr1000", username, mqtt_password))
+  {
     SerialUSB.print(".");
     delay(500);
   }
   SerialUSB.println("\nMQTT connection successful!");
 }
 
-void messageCallback(String &topic, String &payload) {
-  SerialUSB.println("Received message '"+payload+"' from topic "+topic);
+void messageCallback(String &topic, String &payload)
+{
+  SerialUSB.println("Received message '" + payload + "' from topic " + topic);
 }
 
-void setup() {
+void setup()
+{
+  SerialUSB.begin(9600);
+  while (!SerialUSB)
+    ;
   Serial1.begin(9600);
-  while (!Serial.available());
-  if(SERVER_ON) {
+  while (!Serial1)
+    ;
+  if (SERVER_ON)
+  {
     set_up_network();
     mqtt_client.subscribe("teamE/test/receive");
   }
 }
 
-void loop() {
-  while(!Serial1.available());
+void loop()
+{
   String msg = Serial1.readString();
-  int id, max_t, min_t, avg_t;
-  state_enum state_tmp;
   char msg_str[15];
   msg.toCharArray(msg_str, 15);
+
+  int id, max_t, min_t, avg_t;
+  int state_tmp;
   sscanf(msg_str, "%d,%d,%d,%d,%d", &id, &state_tmp, &max_t, &min_t, &avg_t);
-  state[id] = state_tmp;
-  if (SERVER_ON) {
+
+  SerialUSB.print(id);
+  SerialUSB.print(" ");
+  SerialUSB.print(state_tmp);
+  SerialUSB.print(" ");
+  SerialUSB.print(max_t);
+  SerialUSB.print(" ");
+  SerialUSB.print(min_t);
+  SerialUSB.print(" ");
+  SerialUSB.println(avg_t);
+
+  state[id] = (state_enum)state_tmp;
+
+  if (SERVER_ON)
+  {
     mqtt_client.loop();
-    if (!mqtt_client.connected()) set_up_network();
-    if (millis() - lastMillis > 1000) {
+    if (!mqtt_client.connected())
+      set_up_network();
+    if (millis() - lastMillis > 1000)
+    {
       lastMillis = millis();
       mqtt_client.publish("teamE/test/mkr1000-heartbeat", msg_str);
-    } 
+    }
   }
 }
--- Accenture/Case_1/Nano_RP2040/src/main.cpp
+++ Accenture/Case_1/Nano_RP2040/src/main.cpp
@@ -3,54 +3,84 @@
 
 #define LINE_ID 0
 #define MAX_TMP 40
-#define TA_MS 60000
+#define TA_MS 2000
 
-void reset_temperature();
+enum state_enum
+{
+  NORMAL,
+  ALERT,
+  FAULT,
+  DEBUG
+};
+
 void normal();
 void alert();
 void fault();
 void debug();
 
+void retrieve_temperature();
+void send_message(state_enum state);
+void reset_temperature();
+
+state_enum state;
 unsigned long delay_ms, delay_sum_ms;
 int max_t, min_t, avg_t, sum, n_temperature;
-enum state_enum {NORMAL, ALERT, FAULT, DEBUG};
-state_enum state;
 
-
-void setup() {
+void setup()
+{
   Serial1.begin(9600);
+  while (!Serial1)
+    ;
   // put your setup code here, to run once:
-if (!IMU.begin()) {
+  if (!IMU.begin())
+  {
     Serial.println("Failed to initialize IMU!");
-    while (1);
-  
+    while (1)
+      ;
   }
-  delay_ms = 10000;
   reset_temperature();
+  delay_ms = 500;
   state = NORMAL;
-  if (state == DEBUG) pinMode(LED_BUILTIN, OUTPUT);
+  if (state == DEBUG)
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  switch (state) {
-    case DEBUG : DEBUG(); break;
-    case NORMAL: normal(); break;
-    case ALERT: alert(); break;
-    default: fault(); break;
+void loop()
+{
+  switch (state)
+  {
+  case DEBUG:
+    debug();
+    break;
+  case NORMAL:
+    normal();
+    break;
+  case ALERT:
+    alert();
+    break;
+  default:
+    fault();
+    break;
   }
 }
 
-void normal() {
+void normal()
+{
   // put your main code here, to run repeatedly:
-  delay_ms = 10000;
+  delay_ms = 500;
   delay(delay_ms);
   retrieve_temperature();
+  delay_sum_ms += delay_ms;
+  if (delay_sum_ms >= TA_MS)
+  {
+    send_message(state);
+    reset_temperature();
+  }
   // "id,status,max,min,avg" id = index 0, status index 2, max index 4-5, min index 7-8, avg index 10-11-12-13-14;
-  send_message(state);
-  reset_temperature();
-    }
-  
-void retrieve_temperature() {
+}
+
+void retrieve_temperature()
+{
   if (IMU.temperatureAvailable())
   {
     int temperature_deg = 0;
@@ -60,69 +90,88 @@ void retrieve_temperature() {
     Serial.print(temperature_deg);
     Serial.println(" °C");
 
-    if (temperature_deg > MAX_TMP) {
+    if (temperature_deg > MAX_TMP)
+    {
       state = ALERT;
-    
     }
-    else {
+    else
+    {
       state = NORMAL;
-
-    } 
-    n_temperature++;
-    sum+=temperature_deg;
-    if (temperature_deg > max_t) max_t = temperature_deg;
-    if (temperature_deg < min_t) min_t = temperature_deg;
-    delay_sum_ms+=delay_ms;
-    if ( delay_sum_ms >= TA_MS) {
-      int avg_t = sum / n_temperature;
-      Serial.print("Recap last minute: MAX = ");
-      Serial.print(max_t);
-      Serial.print(" °C");
-      Serial.print(", MIN = ");
-      Serial.print(min_t);
-      Serial.print(" °C");
-      Serial.print(", AVG = ");
-      Serial.print(avg_t);
-      Serial.println(" °C");
     }
-} 
+    n_temperature++;
+    sum += temperature_deg;
+    if (temperature_deg > max_t)
+      max_t = temperature_deg;
+    if (temperature_deg < min_t)
+      min_t = temperature_deg;
+  }
 }
 
-void send_message(state_enum state) {
+void send_message(state_enum state)
+{
+  avg_t = sum / n_temperature;
+
+  SerialUSB.print("Recap last minute: MAX = ");
+  SerialUSB.print(max_t);
+  SerialUSB.print(" °C");
+  SerialUSB.print(", MIN = ");
+  SerialUSB.print(min_t);
+  SerialUSB.print(" °C");
+  SerialUSB.print(", AVG = ");
+  SerialUSB.print(avg_t);
+  SerialUSB.println(" °C");
+
   char buffer[15];
-  switch (state) {
-    case NORMAL : sprintf(buffer, "%d,%d,%d,%d,%.2f", LINE_ID, state, max_t, min_t, avg_t); break;
-    case ALERT: case FAULT : sprintf(buffer, "%d,%d,0,0,0.00", LINE_ID, state); break;
-    default : return;
-}
+  switch (state)
+  {
+  case NORMAL:
+    sprintf(buffer, "%d,%d,%d,%d,%d", LINE_ID, state, max_t, min_t, avg_t);
+    break;
+  case ALERT:
+  case FAULT:
+    sprintf(buffer, "%d,%d,0,0,0", LINE_ID, state);
+    break;
+  default:
+    return;
+  }
+
+  SerialUSB.print("Sending buffer: ");
+  SerialUSB.println(buffer);
+
   Serial1.write(buffer);
 }
 
-void reset_temperature() {
-  max_t = 0;
-  min_t = 200;
+void reset_temperature()
+{
+  max_t = -100;
+  min_t = 100;
   sum = 0;
-  n_temperature= 0;
+  n_temperature = 0;
   delay_sum_ms = 0;
 }
 
-void alert() {
-  delay_ms = 1000;
+void alert()
+{
+  delay_ms = 100;
   delay(delay_ms);
   retrieve_temperature();
   send_message(state);
-  reset_temperature
+  reset_temperature();
+
   Serial.println("ALERT");
-  
 }
 
-void fault() {
+void fault()
+{
   send_message(state);
-  while (1) ; //line shut_down
+  while (1)
+    ; // line shut_down
 }
-void debug() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+
+void debug()
+{
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(1000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+  delay(1000);                     // wait for a second
 }
